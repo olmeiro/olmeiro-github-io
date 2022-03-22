8980446aafadcbb461f53531eb7e0c5099660be0
Olmeiro orozco <olme.orozco@gmail.com>
2022-03-22 11:28:57 -0500

    testing deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Vamos Olmeiro tu puedes!
         </a>
       </header>
     </div>
